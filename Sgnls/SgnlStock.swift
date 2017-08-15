24f40b10c9c0e1958d781532b6b5792f42eafb2b
Jesse Tello <jesse.tello@giift.com>
2017-08-15 15:15:13 -0400

    request

--- Sgnls/SgnlStock.swift
+++ Sgnls/SgnlStock.swift
@@ -13,5 +13,18 @@ typealias AnyDict = [String: Any]
 class SgnlStock {
     let symbol: String
     let name: String
-    let type: SgnlType
+    let intraday: [String:Any]
+    
+    init?(dictionary: AnyDict) {
+        guard let metaDict = dictionary["Meta Data"] as? AnyDict,
+            let intraday = dictionary["Time Series"] as? AnyDict,
+            let symbol = metaDict["Symbol"] as? String,
+            let name = metaDict["Information"] as? String
+            else {
+                return nil
+        }
+        self.symbol = symbol
+        self.name = name
+        self.intraday = intraday
+    }
 }
--- Sgnls/SgnlStockViewModel.swift
+++ Sgnls/SgnlStockViewModel.swift
@@ -20,13 +20,14 @@ enum SgnlType: Int32 {
 class SgnlStockViewModel {
     
     let stock:SgnlStock
-    
+    fileprivate let bag = DisposeBag()
+
     init(sgnlStock:SgnlStock) {
         self.stock = sgnlStock
     }
     
     func intradayData() {
-        let req = URLRequest(url: URL(string: "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + self.stock.symbol! + "&interval=1min&apikey=\(Constants.apiKey)")!)
+        let req = URLRequest(url: URL(string: "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + self.stock.symbol + "&interval=1min&apikey=\(Constants.apiKey)")!)
         dataRequest(request: req)
     }
     
@@ -39,7 +40,29 @@ class SgnlStockViewModel {
     }
     
     private func dataRequest(request:URLRequest) {
-        let responseJSON = URLSession.shared.rx.json(request: request)
+        let responseJSON = URLSession.shared.rx.json(request: request).flatMap { request-> Observable<(HTTPURLResponse, Data)> in
+             return URLSession.shared.rx.response(request: request as! URLRequest)
+        }
         
+        responseJSON.filter { response, _ in
+                return 200..<300 ~= response.statusCode
+            }
+            .map { _, data -> [[String: Any]] in
+                guard let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []),
+                    let result = jsonObject as? [[String: Any]] else {
+                        return []
+                }
+                return result
+            }
+            .filter { objects in
+                return objects.count > 0
+            }
+            .map { objects in
+                return objects.flatMap(SgnlStock.init)
+            }
+            .subscribe(onNext: { [weak self] newEvents in
+               
+            })
+            .addDisposableTo(bag)
     }
 }
